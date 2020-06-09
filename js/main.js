17ba074a756977ca3f1c5695c0efe9b6952276df
Melanie Linares <mnmclaughlin0@gmail.com>
2020-06-09 18:10:13 -0400

    6_9_2020
    initial commit for placeholder content

---
+++ js/main.js
@@ -0,0 +1,54 @@
+   var now = new Date();
+   var hrs = now.getHours(); //getHours takes visitors local time, not server time.
+   var msg = "";
+   
+   if (hrs > 5) msg = "and good morning"; // After 6am
+   else if (hrs > 12) msg = "and good afternoon"; // After 12pm
+   else if (hrs > 17) msg = "and good evening"; // After 5pm
+   else msg = "you night owl"; // After 10pm
+   
+   document.getElementById("msg").innerHTML = msg;
+   
+   function blink() {
+    loopBlinkingInRandomCountDownTime();
+   }
+   
+   function loopBlinkingInRandomCountDownTime() {
+    const MIN_TIME_IN_SECOND = 3;
+    const MAX_TIME_IN_SECOND = 10;
+    setTimeout(
+     blink,
+     getRandomCountDownTimeBetween(MIN_TIME_IN_SECOND, MAX_TIME_IN_SECOND)
+    );
+   }
+   
+   function getRandomCountDownTimeBetween(min, max) {
+    const MILLISECOND = 1000;
+    return (
+     Math.floor(Math.random() * (max - min)) * MILLISECOND +
+     min * MILLISECOND
+    );
+   }
+   loopBlinkingInRandomCountDownTime();
+   const SWITCH_WORD_IN_SECOND = 4000;
+   const wordElement = document.getElementById("word");
+   const words = ["scruffy dogs", "board games", "CMYK", "papercraft", "jazz", "dark chocolate"];
+   setInterval(function() {
+    randomIndex = Math.floor(Math.random() * words.length);
+    wordElement.innerText = words[randomIndex];
+   }, SWITCH_WORD_IN_SECOND);
+
+function Blinkinglogo() {
+  setInterval(function () { 
+    blink();
+  }, 10000);
+}
+function blink() {
+  // note no timeout for the hiding part
+  document.getElementById('blink').classList.add("imageblink");
+  setTimeout(function () {
+    document.getElementById('blink').classList.remove("imageblink");
+  }, 150);
+}
+
+Blinkinglogo();
---
+++ js/readme.js
@@ -0,0 +1,5 @@
+console.log(
+    "%cPage last updated:",
+    "color:#537D88;font-family:system-ui;font-size:1rem;font-weight:bold",
+    "06/09/2020"
+   );
